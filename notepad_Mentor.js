c59f9e7ff4d3fe6e788e64101e6c2bd0afd56d8f
Teo Nogueira <teonogueira17@gmail.com>
2021-05-31 12:58:38 -0300

    first commit

---
+++ notepad_Mentor.js
@@ -0,0 +1,29 @@
+/*
+
+técnica de pomodoro, 20 min de estudo e 5 de descanso
+
+# fazer conexões (um ponto bom para softskills)
+
+# participar da comunidade RocketSeat... etc.
+
+# mentalidade no aprendizado
+
+# descanso, alimentação, dormir bem, persistência
+
+# vença a síndrome do impostor: "Não sou tão bom quanto fulano.. etc..".
+
+
+*/
+
+// display flex tornar um ao lado do outro
+
+
+
+
+
+
+
+
+
+
+
